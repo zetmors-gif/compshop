9a628afac99c809cfbe69b9b6d32208be544caeb
ZetMors <zetmors@gmail.com>
2026-02-12 11:00:08 +0300

    Configure React app for deployment to GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -14,18 +14,19 @@ const ZetMorsUltraWide = () => {
     <div className="max-w-375 bg-[#050505] text-white">
       <Header />
       
-      {/* 1. HERO SECTION - FULLSCREEN */}
-      <TopMenu />
+      <div className='flex justify-center'>
+        {/* 1. HERO SECTION - FULLSCREEN */}
+        <TopMenu />
+      </div>
+        {/* 2. PRICES - FULL WIDTH GRID */}
+        <Price />
+        <Hall />
 
-      {/* 2. PRICES - FULL WIDTH GRID */}
-      <Price />
-      <Hall />
+        {/* 3. REVIEWS - FULLSCREEN DARK BLOCK */}
+        <Review />
 
-      {/* 3. REVIEWS - FULLSCREEN DARK BLOCK */}
-      <Review />
-
-      {/* 4. FOOTER - FULL WIDTH RED FOOTER */}
-      <Footer />
+        {/* 4. FOOTER - FULL WIDTH RED FOOTER */}
+        <Footer />
     </div>
   );
 };
